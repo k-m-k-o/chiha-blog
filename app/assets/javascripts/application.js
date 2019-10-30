0f7fb0c108424889b0e1143f67b563e3652f9828
kazuki mokuo <sp9t5ns9@icloud.com>
2019-10-30 18:45:44 +0900

    add application data and responsive function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,36 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
   $('.carousel').carousel();
 });
+
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay:true, 
+    dots:true
+  });
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
