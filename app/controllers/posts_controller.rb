fa313bc1d73113612a8e1e98621fb163d7c96e1b
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-08 00:03:19 +0900

    add routes and controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def show
+
+  end
+
+  def all
+
+  end
+
+  def fishing
+
+  end
+
+  def audio
+
+  end
+
+  def tech
+
+  end
+
+  def others
+    
+  end
+end
