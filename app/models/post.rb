81835bc7d7d1fe051c10a00161bb8b83a510030a
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-14 00:12:30 +0900

    add_option_dependent_destroy_to_model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :admin
   mount_uploader :thumb, ThumbUploader
   has_many :tags,through: :post_tags
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
 end
