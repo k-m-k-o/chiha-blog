8849f510724569d3323b56c859f951756f7de025
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-16 13:08:23 +0900

    updated image-upload-function

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -8,15 +8,11 @@ class Asset
   BasePath   = 'assets/'
 
   def save
+    s3 = Aws::S3::Resource.new(region: "ap-northeast-1")
     filename = Time.zone.now.strftime('%Y%m%d%H%M%S%6N') + File.extname(@file.original_filename)
     obj = s3.bucket(BucketName).object(BasePath + filename)
     obj.upload_file(@file.tempfile)
     @url = obj.public_url(virtual_host: true).gsub(/^http:\/\/#{BucketName}/, "https://#{ENV['AWS_S3_PASS']}")
   end
 
-  private
-
-  def s3
-    @s3 ||= Aws::S3::Resource.new(region: "ap-northeast-1")
-  end
 end
